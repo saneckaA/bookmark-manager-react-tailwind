1c9124881d2ff5da4668ddc5334854c5dfab1dfd
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-13 16:22:48 +0100

    added faq component

---
+++ src/FAQSection/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const FAQSection = () => {
+    return (
+        <section id='faq' className='mt-10'>
+            <div className='mx-auto'>
+                <h2 className='text-3xl md:text-4xl font-semibold text-center'>Frequently Asked Questions</h2>
+                <p className='text-grayishBlue text-center max-w-lg mx-auto mt-5'>
+                    Here are some of our FAQs. If you have any other questions you'd like
+                    answered please feel free to email us.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default FAQSection;
