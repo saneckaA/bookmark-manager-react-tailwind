82cbc10ac0d3b4690ecde2e51f848f40a52439ea
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-13 16:23:36 +0100

    added faq according component

---
+++ src/FAQAccording/index.js
@@ -0,0 +1,118 @@
+import React from 'react'
+
+const FAQAccording = () => {
+    return (
+        <section id='faq-according'>
+            <div className='mx-auto px-6 mb-32'>
+                <div className='max-2-xl m-8 mx-auto overflow-hidden'>
+                    <div className='py-1 border-b outline-none group' tabIndex="1">
+                        <div className='flex items-center justify-between py-3 text-gray-500
+                             transition duration-500 cursor-pointer group ease'>
+                            <div className='transition duration-500 ease group-hover:text-red-500'>
+                                What is Bookmark?
+                            </div>
+                            <div className='transition duration-500 ease group-focus:-rotate-180
+                             group-focus:text-red-500'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12">
+                                    <path fill='none' stroke='currentColor' stroke-width='3' d='M1 1l8 8 8-8' />
+                                </svg>
+                            </div>
+                        </div>
+                        <div className='overflow-hidden transition duration-500 group-focus:max-h-screen
+                           max-h-0 ease'>
+                            <p className='py-2 text-justify text-gray-400'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                                id est laborum.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='py-1 border-b outline-none group' tabIndex="2">
+                        <div className='flex items-center justify-between py-3 text-gray-500
+                             transition duration-500 cursor-pointer group ease'>
+                            <div className='transition duration-500 ease group-hover:text-red-500'>
+                                How can I request a new browser?
+                            </div>
+                            <div className='transition duration-500 ease group-focus:-rotate-180
+                             group-focus:text-red-500'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12">
+                                    <path fill='none' stroke='currentColor' stroke-width='3' d='M1 1l8 8 8-8' />
+                                </svg>
+                            </div>
+                        </div>
+                        <div className='overflow-hidden transition duration-500 group-focus:max-h-screen
+                           max-h-0 ease'>
+                            <p className='py-2 text-justify text-gray-400'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                                id est laborum.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='py-1 border-b outline-none group' tabIndex="3">
+                        <div className='flex items-center justify-between py-3 text-gray-500
+                             transition duration-500 cursor-pointer group ease'>
+                            <div className='transition duration-500 ease group-hover:text-red-500'>
+                                Is there a mobile app?
+                            </div>
+                            <div className='transition duration-500 ease group-focus:-rotate-180
+                             group-focus:text-red-500'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12">
+                                    <path fill='none' stroke='currentColor' stroke-width='3' d='M1 1l8 8 8-8' />
+                                </svg>
+                            </div>
+                        </div>
+                        <div className='overflow-hidden transition duration-500 group-focus:max-h-screen
+                           max-h-0 ease'>
+                            <p className='py-2 text-justify text-gray-400'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                                id est laborum.
+                            </p>
+                        </div>
+                    </div>
+                    <div className='py-1 border-b outline-none group' tabIndex="4">
+                        <div className='flex items-center justify-between py-3 text-gray-500
+                             transition duration-500 cursor-pointer group ease'>
+                            <div className='transition duration-500 ease group-hover:text-red-500'>
+                                What about other Chromium browsers?
+                            </div>
+                            <div className='transition duration-500 ease group-focus:-rotate-180
+                             group-focus:text-red-500'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12">
+                                    <path fill='none' stroke='currentColor' stroke-width='3' d='M1 1l8 8 8-8' />
+                                </svg>
+                            </div>
+                        </div>
+                        <div className='overflow-hidden transition duration-500 group-focus:max-h-screen
+                           max-h-0 ease'>
+                            <p className='py-2 text-justify text-gray-400'>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+                                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                                id est laborum.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default FAQAccording;
