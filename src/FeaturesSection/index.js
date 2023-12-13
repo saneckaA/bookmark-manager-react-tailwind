13402a39057832253bacd69e733ae8cc10a3073e
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-13 16:18:55 +0100

    added features component

---
+++ src/FeaturesSection/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FeaturesSection = () => {
+    return (
+        <section id='features'>
+            <div className='px-6 mx-auto'>
+                <h2 className='text-4xl font-semibold text-center pb-6'>Features</h2>
+                <p className='text-center max-w-md mx-auto text-grayishBlue'>
+                    Our aim is to make it quick and easy for you to access your favourite websites.
+                    Your bookmarks sync between your devices so you can access them on the go.
+                </p>
+            </div>
+        </section>
+
+    )
+}
+
+export default FeaturesSection;
