ac840b8e3e6ede75fb423fe697ef05e7229af631
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-13 23:03:57 +0100

    added routes from react router

--- src/MainContainer/index.js
+++ src/MainContainer/index.js
@@ -9,20 +9,30 @@ import FAQSection from '../FAQSection';
 import FAQAccording from '../FAQAccording';
 import Newsletter from '../Newsletter';
 import Footer from '../Footer';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Panel1 from '../Panel1';
+import Panel2 from '../Panel2';
 
 const MainContainer = () => {
     return (
         <div className=' overflow-x-hidden'>
+            <HashRouter>
             <Navigation />
             <MainSection />
             <FeaturesSection />
             <TabsSection />
-            <Panel3 />
+            <Routes>
+                <Route path="/" element={<Panel1 />} />
+                <Route path="/panel2" element={<Panel2 />} />
+                <Route path="/panel3" element={<Panel3 />} />
+            </Routes>
+           
             <DownloadSection />
             <FAQSection />
             <FAQAccording />
             <Newsletter />
             <Footer />
+            </HashRouter>
         </div>
     )
 }
