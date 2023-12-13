0695efd1c06019cc5d530faae68781a0ba893d83
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-12-13 16:18:14 +0100

    added navigation component

---
+++ src/Navigation/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../images/logo-bookmark.svg';
+
+const Navigation = () => {
+    return (
+        <div className='mx-auto flex px-6 flex-row items-center justify-between pb-20'>
+            <div className='z-30'>
+                <img id="logo" src={logo} alt='' />
+            </div>
+            <nav className='hidden md:flex flex-row space-x-10 items-center text-grayishBlue'>
+                <a href='#features' className='uppercase tracking-widest hover:text-softRed'>Features</a>
+                <a href='#download' className='uppercase tracking-widest hover:text-softRed'>Download</a>
+                <a href='#faq' className='uppercase tracking-widest hover:text-softRed'>FAQ</a>
+                <button className='py-2 px-8 text-white bg-softRed border border-softRed rounded-lg 
+                shadow-md uppercase hover:text-softRed hover:bg-white'>
+                    Login
+                </button>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Navigation;
